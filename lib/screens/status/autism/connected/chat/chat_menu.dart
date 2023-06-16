66817fef2674530f89b0305fc4d5af087323d839
Sitraka Rakotovao <eressemusic01@hotmail.com>
2023-06-16 10:10:13 +0200

    adding chat_menu.dart

---
+++ lib/screens/status/autism/connected/chat/chat_menu.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import '../../../../../widgets/color_constants.dart';
+import '../../../../authentication.dart';
+
+// importing user details
+import '../../signup/profile_creation/full_name.dart';
+import '../../signup/profile_creation/birthdate.dart';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class ChatMenu extends StatefulWidget {
+  @override
+  _ChatMenuState createState() => _ChatMenuState();
+}
+
+class _ChatMenuState extends State<ChatMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
