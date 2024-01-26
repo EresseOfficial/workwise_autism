1354e578b4bc2b9dc907371147c971f3ae4d57b5
EresseOfficial <eressemusic01@hotmail.com>
2024-01-26 19:55:31 +0100

    updating Flutter tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:workwise_autism/main.dart'; // Remplacez par le chemin vers votre fichier main.dart
+import 'package:workwise_autism/main.dart';
 
 void main() {
   testWidgets('App should build without crashing', (WidgetTester tester) async {
-    // Exécutez l'app et attendez que tout soit construit
-    await tester.pumpWidget(workwise_autism()); // Remplacez MyApp par le nom de votre widget principal
-
-    // Vérifiez si un certain widget est trouvé dans l'arbre des widgets
+    await tester.pumpWidget(workwise_autism());
     expect(find.byType(MaterialApp), findsOneWidget);
   });
 }
