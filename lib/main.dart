5b1ce90b70b4790961aa41164a2498104fd83611
EresseOfficial <eressemusic01@hotmail.com>
2024-01-26 12:10:54 +0100

    downgrading cloud_firestore dependency + installing Sentry

--- lib/main.dart
+++ lib/main.dart
@@ -21,8 +21,20 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:google_sign_in/google_sign_in.dart';
 
+import 'package:sentry_flutter/sentry_flutter.dart';
+
 // initialize firebase
 void main() async {
+  await SentryFlutter.init(
+        (options) {
+      options.dsn = 'https://8a5583a49bc235f68f5edbd9c4cf7bca@o4506637194756096.ingest.sentry.io/4506637195935744';
+      // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+      // We recommend adjusting this value in production.
+      options.tracesSampleRate = 1.0;
+    },
+    appRunner: () => runApp(workwise_autism()),
+  );
+  // Sentry.captureException(Exception('Sentry Test'));
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(workwise_autism());
